f4e5ba3b5ec56202ba5a7e4a8e916d55930fd851
Kotence69 <74174456+Kotence69@users.noreply.github.com>
2020-11-26 22:07:44 +0200

    Add files via upload

---
+++ domashno.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace domashno_iliq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //okolna povyrhnina na prav krygov cilindyr
+           
+            Console.Write("h = ");
+            double h = double.Parse(Console.ReadLine());
+            Console.Write("r = ");
+            double r = double.Parse(Console.ReadLine());
+            Console.Write("V = ");
+            double V = 3.14 * r * r * h;
+            Console.WriteLine(V);
+            Console.ReadLine();
+
+            //pylna povyrhnina na prav krygov cilindyr
+           
+            
+            Console.Write("r = ");
+            double r1 = double.Parse(Console.ReadLine());
+            Console.Write("h = ");
+            double h1 = double.Parse(Console.ReadLine());
+            Console.Write("S = ");
+            double S = 2 * 3.14 * r * h;
+            Console.WriteLine(S);
+            Console.ReadLine();
+
+            // namirane na S1
+            
+            Console.Write("r = ");
+            double r2 = double.Parse(Console.ReadLine());
+            Console.Write("h = ");
+            double h2 = double.Parse(Console.ReadLine());
+            Console.Write("S1 = ");
+            double S1 = 2 * 3.14 * r * (h + r);
+            Console.WriteLine(S1);
+            Console.ReadLine();
+        
+        
+        
+        
+        }
+
+    }
+}
+
